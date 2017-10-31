e7398a1f10bafb2ebcc7e43d0c2966d68c095ab2
bard <bnhovde@gmail.com>
2017-10-31 23:09:58 +0100

    Updated readme

--- README.md
+++ README.md
@@ -16,13 +16,18 @@ TVPARTY provides a few helper methods and events, but it's up to you to build yo
 To create a new game:
 - 1: Create a new folder in the `/Games` directory to hold your game logic.
 - 2: Add any assets to the `/public/assets/[gameName]` folder.
-- 3: Add your game to the `/Constants/games.js` file to make it available to the app.
+- 3: Add your game to the `/Games/games.js` file to make it available to the app.
 - 4: Wrap your main game file in the `gameWrapper` component. (see API below)
 - 5: Go wild!
 
 #### API
 
 The `GameWrapper` is a higher-order component that will provides the following API:
+
+Props:
+```
+gameData
+```
 
 Events:
 ```
--- src/Games/ExampleGame/ExampleGame.js
+++ src/Games/ExampleGame/ExampleGame.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
 
+// Components
+import Screen from './../../Primitives/Screen';
+import { H1, H2 } from './../../Primitives/H';
+import Block from './../../Primitives/Block';
+
+// Containers
 import GameWrapper from './../../Containers/GameWrapper';
 
 class ExampleGame extends Component {
@@ -11,9 +17,13 @@ class ExampleGame extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>ExampleGame!</h1>
-      </div>
+      <Screen>
+        <H1>ExampleGame!</H1>
+        <p>Game code: {this.props.gameData.code}</p>
+        <Block top={1}>
+          <H2>Connected players:</H2>
+        </Block>
+      </Screen>
     );
   }
 }
