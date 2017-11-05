254b8e1445c5ee283f9cc65ad702c1ae473cb81c
bard <bnhovde@gmail.com>
2017-11-05 21:28:26 +0100

    BUILD

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io').createServer(server);
+const io = require('socket.io')(server);
 
 const port = process.env.PORT || 8080;
 
