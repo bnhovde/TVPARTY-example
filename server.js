639d16247ab19fc048e3fb3856b10da1ed490c51
bard <bnhovde@gmail.com>
2017-11-08 23:14:50 +0100

    FIX - updated express routing

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
